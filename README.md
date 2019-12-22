c914d0ec4e9dab083ee8e69fce61ddbd7cc36e0f
Riley Thompson <jr3thomp@edu.uwaterloo.ca>
2019-12-22 02:33:31 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # cellular-automaton
+
+https://cellularautomaton.herokuapp.com
+
+![cellularautomaton](celluarautomaton.png)
+
+A site to explore a variety of different cellular automaton.
+
+To run locally:
+
+```$ git clone https://github.com/rileythomp/cellular-automaton.git```
+
+```$ cd cellular-automaton```
+
+```$ npm i```
+
+```$ npm start```
+
+Then go to localhost:3000 in a browser.
--- views/js/gameoflife.js
+++ views/js/gameoflife.js
@@ -23,6 +23,14 @@ function birth_cell(ev) {
     }
 }
 
+function clear_board_view() {
+    for (let row = 0; row < board_len; ++row) {
+        for (let col = 0; col < board_len; ++col) {
+            board_view.children[row].children[col].style.backgroundColor = white;
+        }
+    }  
+}
+
 function build_board_view() {
     for (let row = 0; row < board_len; ++row) {
         let row_view = document.createElement('tr');
@@ -44,7 +52,6 @@ function build_board_model() {
         let row_model = [];
     
         for (let col = 0; col < board_len; ++col) {
-            let cell_view = document.createElement('td');
             let cell_model = new CellModel(board_model, row, col);
     
             row_model.push(cell_model)
@@ -77,6 +84,29 @@ document.getElementById('reset').addEventListener('click', function() {
     document.getElementById('iterations').innerHTML = count;
     starting = true;
 })
+
+let initializations = [
+    [{x:25, y: 25},{x: 25, y: 24},{x: 25, y: 26},{x: 26, y: 24},{x: 24, y: 25}],
+    [{x:20, y: 25},{x: 21, y: 25},{x: 21, y: 26},{x: 26, y: 24},{x: 25, y: 26}, {x:26,y:26},{x:27,y:26}],
+    [{x:20, y: 26},{x: 21, y: 24},{x: 21, y: 26},{x: 23, y: 25},{x: 24, y: 26}, {x:25,y:26},{x:26,y:26}],
+    [{x:3, y: 1},{x: 3, y: 2},{x: 3, y: 3},{x: 2, y: 3},{x: 1, y: 2}],
+    [{x:44, y: 24},{x: 44, y: 25},{x: 44, y: 26},{x: 45, y: 23},{x: 45, y: 26}, {x:46,y:26},{x:47,y:26}, {x: 48, y: 25}, {x: 48, y: 23}],
+    [{x:43, y: 27},{x:43, y: 26},{x:43, y: 25},{x: 44, y: 27},{x: 44, y: 24},{x: 45, y: 27},{x: 46, y: 27}, {x:46,y:23},{x:47,y:27}, {x: 48, y: 26}, {x: 48, y: 24}],
+    [{x:42, y: 27},{x:42, y: 26},{x:42, y: 25},{x: 43, y: 27},{x: 43, y: 24},{x: 44, y: 27},{x: 45, y: 27}, {x:45,y:23},{x: 46, y: 27}, {x:46,y:23},{x:47,y:27}, {x: 48, y: 26}, {x: 48, y: 24}],
+];
+
+document.getElementById('initializations').onchange = function() {
+    let initialization_index = Number(document.getElementById('initializations').value);
+    let initialization = initializations[initialization_index];
+    board_model = [];
+    build_board_model();
+    clear_board_view();
+    for (let i = 0; i < initialization.length; ++i) {
+        let coord = initialization[i];
+        board_model[coord.y][coord.x].birth();
+        board_view.children[coord.y].children[coord.x].style.backgroundColor = green;
+    }
+}
 
 document.getElementById('toggle-start').addEventListener('click', function(_ev) {
     if (this.innerHTML == 'Start') {
